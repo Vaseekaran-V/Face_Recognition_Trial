a65adca996ffd75ee756596d45eb7b1f8d5f4e8b
Vaseekaran Varatharajah <vvasee1996@gmail.com>
2020-11-09 10:59:30 +0530

    Finalized model | Added new Folder of Data

--- face_recognition.py
+++ face_recognition.py
@@ -1,5 +1,5 @@
 from keras.layers import Input, Lambda, Dense, Flatten
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
@@ -27,3 +27,68 @@ for layer in vgg.layers:
 
 #useful for getting number of classes
 folders = glob('Datasets/Train/*')
+
+#layers that are going to be trained
+x = Flatten()(vgg.output)
+#x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation = 'softmax')(x)
+
+#creating a model
+model = Model(inputs = vgg.input, outputs = prediction)
+
+#model summary
+model.summary()
+
+#choosing cost and optimization
+model.compile(
+    loss = 'categorical_crossentropy',
+    optimizer = 'adam',
+    metrics = ['accuracy']
+)
+
+train_datagen = ImageDataGenerator(
+    rescale = 1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+    'Datasets/Train',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode = 'categorical'
+)
+
+test_set = test_datagen.flow_from_directory(
+    'Datasets/Test',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode = 'categorical'
+)
+
+#fitting the model
+r = model.fit_generator(
+    training_set,
+    validation_data=test_set,
+    epochs=5,
+    steps_per_epoch=len(training_set),
+    validation_steps=len(test_set)
+)
+
+#displaying the loss and accuracies
+plt.plot(r.history['loss'], label = 'train loss')
+plt.plot(r.history['val_loss'], label = 'val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_Loss')
+
+plt.plot(r.history['acc'], label = 'train accuracy')
+plt.plot(r.history['val_acc'], label = 'val accuracy')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
+
+#saving the model
